1e0d8fc94564dacd0f84856370311197f48c5de1
Bossun Wang <vvmodouco@gmail.com>
2022-12-02 15:13:21 +0800

    evaluation new testing images and report on Readme

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ original repository from https://github.com/EndyWon/AesUST
 
 # training record
 * v1 using original setting, but got artifact and repetitive pattern
+* v2 adjust gan_weight smaller and reduce to occur artifact
+* v3 training with content encoder and get the best content fidelity and hold another
+
+# Evaluation
+
+| version | content fidelity | global effect |    local patterns     |
+|:-------:| :---------: | :---------------: |:---------------------:|
+|   v1    |     0.606484     |       0.810894        |       0.599884        |
+|   v2    |     0.596819     |        0.864735        |       0.594486        |
+|   v3    |     0.639204     |        0.863310        |       0.584813        |
--- evaluation/eval.sh
+++ evaluation/eval.sh
@@ -11,3 +11,10 @@ python quality_criteria.py \
 --style /media/glory/46845c74-37f7-48d7-8b72-e63c83fa4f68/Style_Transfer/AesUST/inputs/select_style \
 --stylized /media/glory/46845c74-37f7-48d7-8b72-e63c83fa4f68/Style_Transfer/AesUST_improvement/train_log_v2/evaluation_outputs/ \
 --output_log AesUST_v2_evaluation_outputs.txt
+
+python quality_criteria.py \
+--device gpu \
+--content /media/glory/46845c74-37f7-48d7-8b72-e63c83fa4f68/Style_Transfer/AesUST/inputs/select_content \
+--style /media/glory/46845c74-37f7-48d7-8b72-e63c83fa4f68/Style_Transfer/AesUST/inputs/select_style \
+--stylized /media/glory/46845c74-37f7-48d7-8b72-e63c83fa4f68/Style_Transfer/AesUST_improvement/train_log_v3/evaluation_outputs/ \
+--output_log AesUST_v3_evaluation_outputs.txt
